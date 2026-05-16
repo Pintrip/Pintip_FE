f8fc3ccd4bac04425176778df58e6ff8647b91d7
hycho04 <hycho8901@gmail.com>
2026-05-16 22:48:02 +0900

    Feat: Place_QuestSelect 헤더 UI 구현

--- src/page/Place_QuestSelect.jsx
+++ src/page/Place_QuestSelect.jsx
@@ -1,11 +1,11 @@
 function Place_QuestSelect() {
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-[#ffffff]">
       {/* Header */}
-      <div className="relative h-14 px-5 my-3 flex items-center">
+      <div className="relative h-14 flex items-center px-[25px]">
         {/* Back Button */}
         <button className="w-6 h-6 flex items-center justify-center">
-          <img src="/arrow_back_ios.png" alt="뒤로가기" className="w-2.5 h-5" />
+          <img src="/arrow_back_ios.png" alt="뒤로가기" className="w-6 h-6" />
         </button>
 
         {/* Title */}
@@ -14,10 +14,9 @@ function Place_QuestSelect() {
             absolute
             left-1/2
             -translate-x-1/2
-            text-xl
+            text-body-1
             font-semibold
             text-[#242424]
-            leading-8
           "
         >
           세션 만들기
